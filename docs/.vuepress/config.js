e514309214637e749b73d163e340e831b3f9f4e3
zero <git>
2022-02-13 22:15:27 +0800

    增加奇安信

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,37 +2,67 @@ module.exports = {
     title: 'ZERO\'s blog',
     description: '我的个人博客网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', {rel: 'icon', href: '/logo.jpg'}], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
-      lineNumbers: false // 代码块显示行号
+        lineNumbers: false // 代码块显示行号
     },
     themeConfig: {
-      nav:[ // 导航栏配置
-        {
-          text: '编程', 
-          link: '/IT/' },
-        {
-          text: '心理学', 
-          link: '/psychology/'},
-        {
-          text: '杂物间', 
-          link: '/utilityRoom/'}      
-      ],
-      sidebar:{
-        '/IT/':[
-          '',
-          'git',
-          'domToImage',
-          'WindowsTerminal'
+        nav: [ // 导航栏配置
+            {
+                text: '编程',
+                link: '/IT/'
+            },
+            {
+                text: '心理学',
+                link: '/psychology/'
+            },
+            {
+                text: '杂物间',
+                link: '/utilityRoom/'
+            }
         ],
+        sidebar: {
+            '/IT/': [
+                {
+                    title: '栏目碎碎念',
+                    children: [
+                        ''
+                    ],
+                },
+                {
+                    title: 'JS',
+                    children: [
+                        'domToImage',
+                    ],
+                },
+                {
+                    title: '开发软件配置',
+                    children: [
+                        'WindowsTerminal'
+                    ],
+                },
+                {
+                    title: 'git',
+                    children: [
+                        'git'
+                    ],
+                },
+            ],
 
-        '/psychology/':[
-          '',
-        ]
-      },
-      // sidebar: 'auto', // 侧边栏配置
-      sidebarDepth: 2, // 侧边栏显示2级
+            '/psychology/': [
+                '',
+            ],
+
+            '/utilityRoom/': [
+                '',
+                '奇安信'
+            ]
+
+
+        },
+        // sidebar: 'auto', // 侧边栏配置
+        sidebarDepth: 2, // 侧边栏显示2级
     }
-  };
+};
