947ddc5c90801a1a7c9687becd67b57c4467f6a7
林奕渠 <1131041765@qq.com>
2022-02-14 09:43:55 +0800

    增加图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    title: 'ZERO\'s blog',
+    title: 'ZERO',
     description: '我的个人博客网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-        ['link', {rel: 'icon', href: '/logo.jpg'}], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', {rel: 'icon', href: '/logo/fox.png'}], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
